a61ea718d46b5ef2aa26f944d06387bece654a08
Tewodros Amare <teddygizachew1@gmail.com>
2021-09-12 20:28:37 -0400

    Add Log error statement

--- app/src/main/java/com/nkufall2021/helloworld/MainActivity.java
+++ app/src/main/java/com/nkufall2021/helloworld/MainActivity.java
@@ -7,10 +7,16 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MyActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("MainActivity", "Hello World");
+        // Log statement to debug
+        Log.d(TAG, "Hello World");
+
+        // Log statement to handle errors
+        Log.e(TAG, "Message was not delivered");
     }
 }
